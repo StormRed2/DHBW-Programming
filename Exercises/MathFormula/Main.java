e0d9a456f842142b6b17eabf38100691fbc0b7ec
Florian Martin <florian.martin91@gmail.com>
2014-11-07 17:13:32 +0100

    Update Main.java

--- Exercises/MathFormula/Main.java
+++ Exercises/MathFormula/Main.java
@@ -1,1 +1,51 @@
-Code here
+package edu.dhbw.mathformular;
+
+import java.text.DecimalFormat;
+
+/**
+ *  Copyright Florian Martin
+ *  Nov 6, 2014
+ *
+ */
+public class MathFormular {
+
+	private static double calculate (int value){
+		
+		double actualValue = 0.0;
+		
+		
+		if (value == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return actualValue = calculate(value - 1)+ Math.pow(1.25, value);
+		}
+		//return actualValue;
+		
+		
+	}
+	
+	// if (i == 0)
+	//{
+	//actualValue = 0.0
+	//}
+	//
+	//for (int i=1; i<=index; i++)
+	//{
+	//actualValue = actualValue + Math.pow(1.25, i)
+	//}
+	//return actualValue
+	//}
+	//      !!abbruchbedingung nicht vergessen!!
+	
+	public static void main(String[] args){
+		
+		calculate(1);
+	
+	DecimalFormat myFormat = new DecimalFormat( "###0.00");
+	System.out.println("Formatiert" +myFormat.format(calculate(10)));
+	
+	}
+}
